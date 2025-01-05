2d4f1964f595a4b074fd9aa50256ec89842c9587
dat <thudatdl123@gmail.com>
2025-01-06 00:17:35 +0700

    add bottom tab in home screen

--- lib/screens/main/home/home.dart
+++ lib/screens/main/home/home.dart
@@ -9,6 +9,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex = 0; // To track the current tab
+  final List<Widget> _pages = [
+    const Center(child: Text('Home', style: TextStyle(fontSize: 24))),
+    const Center(child: Text('Search', style: TextStyle(fontSize: 24))),
+    const Center(child: Text('Profile', style: TextStyle(fontSize: 24))),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +24,29 @@ class _HomeState extends State<Home> {
           content: "Home",
         ),
       ),
-      body: SafeArea(
-          child: Container(
-        child: const Text('This is home screen'),
-      )),
+      body: _pages[_currentIndex], // Display the selected page
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index; // Update the current tab index
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
     );
   }
 }
