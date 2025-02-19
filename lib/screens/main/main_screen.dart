6571ef31b6e691724dfaa7eb46ed78bbc5233647
powdevlin12 <thudatdl123@gmail.com>
2025-02-19 22:48:33 +0700

    style : adjust bottom tab

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -2,6 +2,7 @@ import 'package:ercomerce_app/configs/colors.dart';
 import 'package:ercomerce_app/screens/main/home/home.dart';
 import 'package:ercomerce_app/screens/main/profile/profile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Main extends StatefulWidget {
   const Main({super.key});
@@ -12,18 +13,22 @@ class Main extends StatefulWidget {
 
 class _MainState extends State<Main> {
   int _currentIndex = 0; // To track the current tab
-
+  static const double sizeIcon = 48.0;
   @override
   Widget build(BuildContext context) {
     final List<Widget> pages = [
       const Home(),
       const Center(child: Text('Search', style: TextStyle(fontSize: 24))),
-      const Profile()
+      const Profile(),
+      const Profile(),
     ];
 
     return Scaffold(
       body: pages[_currentIndex], // Display the selected page
       bottomNavigationBar: BottomNavigationBar(
+        iconSize: 30,
+        selectedFontSize: 0,
+        unselectedFontSize: 0,
         currentIndex: _currentIndex,
         backgroundColor: subBgColor,
         onTap: (index) {
@@ -32,18 +37,62 @@ class _MainState extends State<Main> {
           });
         },
         selectedItemColor: primaryColor,
-        items: const [
+        items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+            icon: SvgPicture.asset(
+              "assets/home.svg",
+              width: sizeIcon,
+              height: sizeIcon,
+            ),
+            activeIcon: SvgPicture.asset(
+              "assets/home.svg",
+              width: sizeIcon,
+              height: sizeIcon,
+              color: primaryColor,
+            ),
+            label: '',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
+            icon: SvgPicture.asset(
+              "assets/notification.svg",
+              width: sizeIcon,
+              height: sizeIcon,
+            ),
+            activeIcon: SvgPicture.asset(
+              "assets/notification.svg",
+              width: sizeIcon,
+              height: sizeIcon,
+              color: primaryColor,
+            ),
+            label: '',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
+            icon: SvgPicture.asset(
+              "assets/save.svg",
+              width: sizeIcon,
+              height: sizeIcon,
+            ),
+            activeIcon: SvgPicture.asset(
+              "assets/save.svg",
+              width: sizeIcon,
+              height: sizeIcon,
+              color: primaryColor,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/profile.svg",
+              width: sizeIcon,
+              height: sizeIcon,
+            ),
+            activeIcon: SvgPicture.asset(
+              "assets/profile.svg",
+              width: sizeIcon,
+              height: sizeIcon,
+              color: primaryColor,
+            ),
+            label: '',
           ),
         ],
       ),
