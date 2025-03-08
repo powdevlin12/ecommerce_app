3383fcec516f01aa66f5697e6f04358cf597079e
powdevlin12 <thudatdl123@gmail.com>
2025-03-08 18:45:08 +0700

    feat : add cart model and cart product model

---
+++ lib/models/cart.model.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'dart:convert';
+import 'package:ercomerce_app/models/cart_product.model.dart';
+import 'package:ercomerce_app/utils/parse_type_value.dart';
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class CartModel {
+  final String cartId;
+  final String cart_state;
+  final String cart_userId;
+  final int cart_count_product;
+  final CartProductModel cart_product;
+
+  const CartModel({
+    required this.cartId,
+    required this.cart_state,
+    required this.cart_userId,
+    required this.cart_count_product,
+    required this.cart_product,
+  });
+
+  factory CartModel.fromJson(Map<String, dynamic> json) {
+    return CartModel(
+        cartId: ParseTypeData.ensureString(json["_id"]),
+        cart_state: ParseTypeData.ensureString(json["cart_state"]),
+        cart_userId: ParseTypeData.ensureString(json["cart_userId"]),
+        cart_count_product: ParseTypeData.ensureInt(json["cart_count_product"]),
+        cart_product: CartProductModel.fromJson(json['cart_product']));
+  }
+}
---
+++ lib/models/cart_product.model.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:ercomerce_app/utils/parse_type_value.dart';
+
+class CartProductModel {
+  final String cartProductId;
+  final String productId;
+  final String shopId;
+  final int quantity;
+  final String name;
+  final int price;
+
+  const CartProductModel({
+    required this.cartProductId,
+    required this.productId,
+    required this.shopId,
+    required this.quantity,
+    required this.name,
+    required this.price,
+  });
+
+  factory CartProductModel.fromJson(Map<String, dynamic> json) {
+    return CartProductModel(
+      cartProductId: ParseTypeData.ensureString(json["_id"]),
+      productId: ParseTypeData.ensureString(json["productId"]),
+      shopId: ParseTypeData.ensureString(json["shopId"]),
+      quantity: ParseTypeData.ensureInt(json["quantity"]),
+      name: ParseTypeData.ensureString(json["name"]),
+      price: ParseTypeData.ensureInt(json["price"]),
+    );
+  }
+}
